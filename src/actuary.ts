73c6ace9aac3d5fda8461f1be8cb1da3555ba929
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-07-24 15:31:06 -0700

    Fix JSON imports.

--- src/actuary.ts
+++ src/actuary.ts
@@ -4,7 +4,7 @@
  * Github: https://github.com/BobKerns/retirement-simulator
  */
 
-import SS_2017_raw from './data/SS_2017.json';
+import * as SS_2017_raw from './data/SS_2017.json';
 import { IPerson, Sex } from './types';
 
 
--- src/color.ts
+++ src/color.ts
@@ -6,15 +6,13 @@
 
 import * as d3 from 'd3';
 import {uniq} from 'ramda';
-import { IItem, Name } from './types';
-import raw_color_scheme from './data/color_scheme.json';
+import * as raw_color_scheme from './data/color_scheme.json';
 
 
 export const color_scheme: Array<Color> = raw_color_scheme;
 
 export type Color = string;
 
-// NPM
 export const compute_colors =
     (color_scheme: Array<Color>, unknown = "#000000") =>
     (scenario_list: Scenario[]) =>
