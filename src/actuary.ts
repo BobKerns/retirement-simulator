ee1449fa0f9fd3e41b97743fba1efb37aef484af
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-07-27 08:50:09 -0700

    Actuary: Use correct age, and interpolate.

--- src/actuary.ts
+++ src/actuary.ts
@@ -5,7 +5,9 @@
  */
 
 import * as SS_2017_raw from './data/SS_2017.json';
+import { calculate_age } from './time';
 import { IPerson, Sex } from './types';
+import { Throw } from './utils';
 
 
 export type ActuaryDatum = {
@@ -39,46 +41,40 @@ export type ActuaryAnnualData = {
  */
 export const SS_2017: Array<ActuaryAnnualData> = SS_2017_raw;
 
-export const isLeapYear = (year: number) => {
-    if ((year % 4) !== 0) {
-        return false;
-    } else if ((year % 100) !== 0) {
-        return true;
-    } else if ((year % 400) !== 0) {
-        return false;
-    } else {
-        return true;
-    }
-};
-
-export const yearDays = (year: number) => isLeapYear(year) ? 366 : 365;
-
-const MONTH_START = [
-    [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365],
-    [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
-]; //=
-
-export const dayOfYear = (date: Date) => {
-    const month = date.getUTCMonth();
-    const day =  date.getUTCDate();
-    return MONTH_START[isLeapYear(date.getUTCFullYear()) ? 1 : 0][month] + day;
-}
-
-export const age = (birthday: Date, date: Date) => {
-    const offset = dayOfYear(date) > dayOfYear(birthday) ? -1 : 0
-    return date.getUTCFullYear() - birthday.getUTCFullYear() + offset;
-}
-
+/**
+ * Obtain actuarial data for a person on a particular date.
+ *
+ * Interpolates from the actuarial tables. (Linear interpolation for now, although that is not strictly correct),
+ * @param spouse
+ * @param date The date on which the age is desired.
+ */
 export function actuary(spouse: IPerson, date: Date): ActuaryDatum;
+/**
+ * Obtain actuarial data for a specified age and sex.
+ *
+ * Interpolates from the actuarial tables. (Linear interpolation for now, although that is not strictly correct),
+ * @param age
+ * @param sex
+ */
 export function actuary(age: number, sex: Sex): ActuaryDatum;
 export function actuary(spouseOrAge: IPerson | number, dateOrSex: Date | Sex): ActuaryDatum {
     if (typeof spouseOrAge == 'number') {
-        return SS_2017[Math.round(spouseOrAge)]?.[dateOrSex as Sex];
+        const idx = Math.floor(spouseOrAge);
+        const frac = spouseOrAge - idx;
+        const sex = dateOrSex as Sex;
+        const base = SS_2017[idx]?.[sex] ?? Throw(`No data for age ${idx}.`);
+        if (frac <= 0.003) return base;
+        const next = SS_2017[idx + 1]?.[sex] ?? Throw(`No data for age ${idx + 1}.`);
+        const interpolate = (a: number, b: number, frac: number) => (a * (1 - frac) + b * frac);
+        return {
+            p: interpolate(base.p, next.p, frac),
+            n: Math.round(interpolate(base.n, next.n, frac)),
+            years: interpolate(base.years, next.years, frac)
+        };
     } else {
         const birth = spouseOrAge.birth;
         const date = (dateOrSex as Date);
-        const days = (date.getTime() - birth.getTime()) / (24 * 60 * 60 * 1000);
-        const age = 5;
+        const age = calculate_age(birth, date);
         return actuary(age, spouseOrAge.sex)
     }
 }
