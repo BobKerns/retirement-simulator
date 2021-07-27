e638068b588f9d79c02b7e060c1dc10ac712d845
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-07-27 08:52:36 -0700

    Add simple tests.

---
+++ src/__tests__/test-expense.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2021 by Bob Kerns. Licensed under MIT license.
+ *
+ * Github: https://github.com/BobKerns/retirement-simulator
+ */
+
+import {Expense} from '../expense';
+import { construct } from '../utils';
+import { expense_1 } from './samples';
+
+describe("Expense", () => {
+    test("Create", () => expect(new Expense(expense_1).fromStream).toEqual(expense_1.fromStream));
+    test("Construct", () => expect(construct(expense_1).categories).toEqual(['fred']));
+    test("Default scenario", () => expect(construct(expense_1).scenarios).toEqual(['Default']));
+})
---
+++ src/__tests__/test-item.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2021 by Bob Kerns. Licensed under MIT license.
+ *
+ * Github: https://github.com/BobKerns/retirement-simulator
+ */
+
+import {Expense} from '../expense';
+import { construct } from '../utils';
+import { expense_1 } from './samples';
+
+describe("Items", () => {
+    test("Category yes", () => expect(new Expense(expense_1).hasCategory('fred')).toBeTruthy());
+    test("Category no", () => expect(new Expense(expense_1).hasCategory('sally')).toBeFalsy());
+    test("Default scenario", () => expect(construct(expense_1).scenarios).toEqual(['Default']));
+    test("Non-Default scenario", () => expect(construct({...expense_1, scenarios: ['rich', 'poor']}).scenarios).toEqual(['rich', 'poor']));
+});
