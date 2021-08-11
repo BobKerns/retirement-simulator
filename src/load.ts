3e3beb1343e57aba41c0eda1a70f7c0874baf909
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-08-11 13:05:19 -0700

    loadData returns scenario list.

--- src/load.ts
+++ src/load.ts
@@ -11,8 +11,9 @@
 
 import { csvParse, DSVRowArray } from 'd3-dsv';
 import { convert } from './input';
-import { isString } from './tagged';
-import { InputRow, Type } from './types';
+import { Scenario } from './scenario';
+import { as, isString } from './tagged';
+import { InputRow, RowType, Type } from './types';
 
 export const parse = (text: string) => {
     return csvParse(text);
@@ -44,13 +45,20 @@ export async function loadFrom(urlOrRequest: string | Requested | Promise<string
     return parse(await url.text()).map(r => ({Type: type, ...r, Src: src}));
 };
 
+/**
+ * Load the model data.
+ * @param url URL to an index file
+ * @returns a list of {@link Scenario} objects
+ */
 export const loadData = async (url: string) => {
     const base = url.replace(/[/][^/]+$/, '');
     const files = await loadFrom(url, url, 'index');
     const load = (f: any) => loadFrom(`${base}/${f.File}`, f.File!, f.Type as Type);
     const tables = await Promise.all(files.map(load));
-    return tables
+    const data = tables
         .flatMap(i  => i as unknown as InputRow)
         .map(convert);
+    return data
+        .filter(r => r.type === 'scenario')
+        .map(r => new Scenario(r.name, (data) as RowType[],  as(r.end?.getUTCFullYear() ?? 2080)));
 }
-
