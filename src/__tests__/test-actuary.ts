e35078d9c20aa4efc696fcf84380613d74c78b5b
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-08-13 20:55:31 -0700

    Actuary tests were timezone-sensitive and failing on GitHub.

--- src/__tests__/test-actuary.ts
+++ src/__tests__/test-actuary.ts
@@ -11,8 +11,8 @@ describe("Actuary", () => {
     test("age male", () => expect(actuary(1, 'male')).toEqual({n: 99370, p: 0.000426, years: 75.45}));
     test("age female", () => expect(actuary(1, 'female')).toEqual({n: 99477, p: 0.000342, years: 80.39}));
 
-    const january = new Date(2022, 0, 1);
-    const july = new Date(2022, 6, 3); // Midyear
+    const january = new Date(2022, 0, 1, 0, 0, 0, 0);
+    const july = new Date(2022, 6, 3, 0, 0, 0,0); // Midyear
     test("person male", () => expect(actuary(person_2, january)).toEqual({n: 89345, p: 0.007766, years: 25.5}));
     test("person female", () => expect(actuary(person_1, january)).toEqual({n: 93755, p: 0.004813, years: 28.81}));
 
