665effb409f1bc002ab201b652a862a68b872d77
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-08-20 00:22:31 -0700

    TEST: Use a common person_1 data sample.

--- src/__tests__/test-person.ts
+++ src/__tests__/test-person.ts
@@ -4,24 +4,16 @@
  * Github: https://github.com/BobKerns/retirement-simulator
  */
 
-import { UTC } from "../calendar";
 import { construct } from "../construct";
 import { asYear } from "../tagged";
+import { person_1 } from "./data/samples";
 
 describe('Person', () => {
     test('Simple', () =>
-        expect(construct([{
-                name: 'spouse1',
-                prettyName: "Bob",
-                type: 'person',
-                sex: 'male',
-                categories: [],
-                scenarios: ['Default'],
-                sort: 0,
-                start: UTC(1950),
-                birth: UTC('1950')
-            }],
-            'person', [], asYear(2080)))
-            .toBeDefined());
+        expect(construct(
+            [person_1],
+            'person', [], asYear(2080)
+        ))
+        .toBeDefined());
 
 });
