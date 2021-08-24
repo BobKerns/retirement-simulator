c0ac1cf6c931c16ccb4b3ac0700c00577a222d5f
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-08-24 07:13:50 -0700

    Properly type the value as Money.

--- src/model/income.ts
+++ src/model/income.ts
@@ -9,7 +9,7 @@ import { StateMixin } from "./state-mixin";
 import { IFScenario, IIncome, ItemImpl, ItemState, RowType, Type } from "../types";
 import { classChecks } from "../utils";
 import { CalendarStep } from "../calendar";
-import { asMoney } from "../tagged";
+import { as, asMoney, Money } from "../tagged";
 
 /**
  * An income item. By default, annual, but can be constrained to a particular period of time.
@@ -22,7 +22,7 @@ export class Income extends CashFlow<'income'> implements IIncome {
     *states<T extends Type>(start: CalendarStep): Generator<ItemState<'income'>, any, ItemState<'income'>> {
         let item: ItemImpl<'income'> | null = this as  ItemImpl<'income'>;
         let step = start;
-        let value = 0;
+        let value: Money = as(0);
         while (true) {
             value = asMoney(value + this.value);
             let next = yield {item, step, value};
