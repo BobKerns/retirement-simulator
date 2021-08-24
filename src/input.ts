a688c7a0a0e07b03ae605882595fbc7d403d67bf
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2021-08-23 18:47:55 -0700

    Error check on end wsa backwards.

--- src/input.ts
+++ src/input.ts
@@ -122,7 +122,7 @@ export const convert = <T extends Type>(row: InputRow) => {
         }
     }
     const checkRequired = (list?: Array<keyof AnyRow>) => {
-        if (list && row.End) {
+        if (list && !result.end) {
             for (const k of list) {
                 if (result[k] === undefined || result[k] === '') {
                     const l = upcase(k);
