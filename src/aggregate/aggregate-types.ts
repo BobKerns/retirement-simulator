82810258062561f439fb5cb2026674cebfeb91ce
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2023-06-08 13:52:18 -0700

    Revert "Fix some leftover warnings and type errors."
    This reverts commit ce5d13f05bdf4a2c98c1e9776ed8bb781ea4c6ed.

--- src/aggregate/aggregate-types.ts
+++ src/aggregate/aggregate-types.ts
@@ -43,7 +43,7 @@ export type ValueFn<T, K> = ExtractorFn<T, K>;
  * @type K the type of the keys partitioning the aggregated results
  * @type V the type of value extracted to be passed to the {@link MergeFn}
  */
-export interface AggregationSpec<T extends Record<keyof any, any>, R extends any, K, V extends T> {
+export interface AggregationSpec<T extends Record<keyof any, any>, R extends any, K, V> {
     /**
      * Function to extract the aggregation key, or the name of a field. Defaults to the identity function.
      */
